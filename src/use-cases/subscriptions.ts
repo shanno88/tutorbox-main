4163aaef6bcca67bfc57fc0b4a012f85259d08a1
shanno88 <lkglass@hotmail.com>
2026-03-08 11:21:06 +0800

    Fix use-cases/subscriptions.ts type error

--- src/use-cases/subscriptions.ts
+++ src/use-cases/subscriptions.ts
@@ -1,15 +1,12 @@
-import { getSubscription } from "@/data-access/subscriptions";
+// Subscriptions table removed - using productGrants + trial system instead
+// Use checkUserAccess from @/lib/access for access control
 
-export async function isUserSubscribed(userId: string) {
-  const subscription = await getSubscription(userId);
+export async function isUserSubscribed(_userId: string): Promise<boolean> {
+  // Always return false - subscriptions table no longer exists
+  return false;
+}
 
-  if (!subscription) {
-    return false;
-  }
-
-  if (subscription.currentPeriodEnd < new Date()) {
-    return false;
-  }
-
-  return true;
+export async function hasActiveSubscription(_userId: string): Promise<boolean> {
+  // Always return false - subscriptions table no longer exists
+  return false;
 }
